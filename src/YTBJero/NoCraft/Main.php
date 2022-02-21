415da92abd074f85f08ace774b225bef57598bbc
YTBJero <mw.sevice@gmail.com>
2022-02-21 11:43:16 +0700

    Fix

--- src/YTBJero/NoCraft/Main.php
+++ src/YTBJero/NoCraft/Main.php
@@ -12,21 +12,13 @@ use pocketmine\event\inventory\CraftItemEvent;
 use pocketmine\utils\Config;
 
 use pocketmine\item\Item;
-use pocketmine\item\Tool;
+use pocketmine\item\StringToItemParser;
 
 class Main extends PluginBase implements Listener{
-
-	private static $ids = [];
 
 	public function onEnable(): void{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveResource("config.yml");
-		foreach($this->getConfig()->get("nocraft") as $id => $status){
-			if($this->getConfig()->get("all") === true) return; 
-			if($status === false) return;
-			$ids = explode(":", $id);
-			self::$ids[$ids[0] . ":" . $ids[1]] = true;
-		}
 	}
 
 	public function onCraft(CraftItemEvent $event){
@@ -37,17 +29,14 @@ class Main extends PluginBase implements Listener{
 			$player->sendMessage($config->get("cancel-msg"));
         }
 		foreach ($event->getOutputs() as $item){
-			if (array_key_exists($item->getId() . ":" . $this->getDamage($item), self::$ids)){
-				$event->cancel();
-				$player->sendMessage($config->get("cancel-msg"));
+			foreach($this->getConfig()->get("nocraft") as $name => $status){
+			if($this->getConfig()->get("all") === true) return;
+			if($status === false) return;
+				if($item->equals(StringToItemParser::getInstance()->parse($name), true)){
+					$event->cancel();
+					$player->sendMessage($config->get("cancel-msg"));
+				}
 			}
 		}
     }
-	
-	public function getDamage(Item $item){
-		if($item instanceof Tool){
-			return $item->getDamage();
-		}
-		return 0;
-	}
 }
